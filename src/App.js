0746239b4aeeb1710f0f11c0d1e092bee343078b
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-03-20 14:39:36 -0300

    Add footer component

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import './styles/Header.css';
 
 import MoviesContainer from './containers/MoviesContainer/MoviesContainer';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <Container fluid className="App">
       <Header />
       <MoviesContainer />
+      <Footer />
     </Container>
   );
 }
