3ee0f7cf4ed960340cdc08fc55b340b0aee81929
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-03-21 19:02:46 -0300

    Finish useApi custom hooks

--- src/containers/MovieSelected/MovieSelected.js
+++ src/containers/MovieSelected/MovieSelected.js
@@ -12,29 +12,30 @@ const MovieSelected = () => {
 
   return (
     <Row className="m-auto mt-5">
-      {movieList && movieList.map((movie) => (
-        <Col sm={12} md={6} xl={4} key={movie.id} className="mb-5">
-          <div className="MovieSelected">
-            <Figure>
-              <Figure.Image
-                fluid
-                alt="Movie Poster"
-                src={movie.image}
-              />
-              <Figure.Caption className="text-light">
-                Original title:
-                &nbsp;
-                {movie.original_title}
-              </Figure.Caption>
-            </Figure>
-            <h4>{movie.title}</h4>
-            <p className="moviedescription">{`${movie.description.slice(0, 106)}...`}</p>
-            <Button variant="outline-light" size="md" className="justify-content-end" active>
-              Click to see more...
-            </Button>
-          </div>
-        </Col>
-      )) }
+      {movieList // first movieList is to check if is true before loading
+        && movieList.map((movie) => (
+          <Col sm={12} md={6} xl={4} key={movie.id} className="mb-5">
+            <div className="MovieSelected">
+              <Figure>
+                <Figure.Image
+                  fluid
+                  alt="Movie Poster"
+                  src={movie.image}
+                />
+                <Figure.Caption className="text-light">
+                  Original title:
+                  &nbsp;
+                  {movie.original_title}
+                </Figure.Caption>
+              </Figure>
+              <h4>{movie.title}</h4>
+              <p className="moviedescription">{`${movie.description.slice(0, 106)}...`}</p>
+              <Button variant="outline-light" size="md" className="justify-content-end" active>
+                Click to see more...
+              </Button>
+            </div>
+          </Col>
+        )) }
     </Row>
   );
 };
