7f3381b0334a36733e1e52ca60c0692e03f85e7f
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-03-20 22:31:03 -0300

    Remove text on movies container

--- src/containers/MoviesContainer/MoviesContainer.js
+++ src/containers/MoviesContainer/MoviesContainer.js
@@ -2,15 +2,6 @@ import React from 'react';
 import { Row } from 'react-bootstrap';
 import MovieSelected from '../MovieSelected/MovieSelected';
 
-const MoviesContainer = () => {
-  const containerText = 'Studio Ghibli’s films are undeniably works of art.';
-
-  return (
-    <Row>
-      <h2>{containerText}</h2>
-      <MovieSelected />
-    </Row>
-  );
-};
+const MoviesContainer = () => (<Row><MovieSelected /></Row>);
 
 export default MoviesContainer;
