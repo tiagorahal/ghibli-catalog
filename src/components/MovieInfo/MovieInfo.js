bee1b49a6461e8e3d2efaebde719562f49dc6e2b
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-03-21 19:51:01 -0300

    Pass info by props to MovieInfo

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,53 +1,38 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MovieInfo = (props) => {
-  const movieInfo = {
-    title: props.title,
-    original_title: props.original_title,
-    original_title_romanised: props.original_title_romanised,
-    description: props.description,
-    director: props.director,
-    producer: props.producer,
-    release_date: props.release_date,
-    running_time: props.running_time,
-    rt_score: props.rt_score,
-  };
-
-  return (
-    <div className="MovieInfo">
-      <h1>
-        {movieInfo.title}
-      </h1>
-      <p>
-        {movieInfo.original_title}
-      </p>
-      <p>
-        {movieInfo.original_title_romanised}
-      </p>
-      <p>
-        {movieInfo.description}
-      </p>
-      <p>
-        {movieInfo.director}
-      </p>
-      <p>
-        {movieInfo.producer}
-      </p>
-      <p>
-        {movieInfo.release_date}
-      </p>
-      <p>
-        {movieInfo.running_time}
-      </p>
-      <p>
-        {movieInfo.rt_score}
-      </p>
-    </div>
-  );
-};
+const MovieInfo = (props) => (
+  <div className="MovieInfo">
+    <h1>
+      {props.title}
+    </h1>
+    <p>
+      {props.originalTitle}
+    </p>
+    <p>
+      {props.originalTitleRomanised}
+    </p>
+    <p>
+      {props.description}
+    </p>
+    <p>
+      {props.director}
+    </p>
+    <p>
+      {props.producer}
+    </p>
+    <p>
+      {props.releaseDate}
+    </p>
+    <p>
+      {props.runningTime}
+    </p>
+    <p>
+      {props.rtScore}
+    </p>
+  </div>
+);
 
 MovieInfo.propTypes = ({
   title: PropTypes.string,
--- src/containers/MovieSelected/MovieSelected.js
+++ src/containers/MovieSelected/MovieSelected.js
@@ -5,6 +5,8 @@ import {
   Figure,
   Button,
 } from 'react-bootstrap';
+
+import MovieInfo from '../../components/MovieInfo/MovieInfo';
 import useApi from '../../services/useApi';
 
 const MovieSelected = () => {
@@ -15,7 +17,7 @@ const MovieSelected = () => {
     <Row className="m-auto mt-5">
       {movieList // first movieList is to check if is true before loading
         && movieList.map((movie) => (
-          <Col sm={12} md={6} xl={4} key={movie.id} className="mb-5">
+          <Col sm={12} md={6} xl={4} key={movie.title} className="mb-5">
             <div className="MovieSelected">
               <Figure>
                 <Figure.Image
@@ -35,6 +37,17 @@ const MovieSelected = () => {
                 Click to see more...
               </Button>
             </div>
+            <MovieInfo
+              title={movie.title}
+              originalTitle={movie.original_title}
+              originalTitleRomanised={movie.original_title_romanised}
+              description={movie.description}
+              director={movie.director}
+              producer={movie.producer}
+              releaseDate={movie.release_date}
+              runningTime={movie.running_time}
+              rtScore={movie.rt_score}
+            />
           </Col>
         )) }
     </Row>
